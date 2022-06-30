ac20bec63382a74676b2d255b0c2b97fc73481d3
Floaty_KT <kasemtan88@hotmail.co.th>
2022-06-30 21:43:43 +0700

    feat: add all things

--- quizzzzz/components/Question.jsx
+++ quizzzzz/components/Question.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import styles from "../styles/Question.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+const Question = ({ ...props }) => {
+  const questions = [
+    { q: "Are you gay?" },
+    { q: "Hello?" },
+    { q: "Float" },
+    { q: "1" },
+  ];
+  const [qNumber, setQNumber] = React.useState(0);
+  const renderLeft = React.useMemo(
+    () => (
+      <div id="question" className="w-screen flex justify-center items-center">
+        {questions[qNumber].q}
+      </div>
+    ),
+    [qNumber]
+  );
+  const renderRight = React.useMemo(() => {
+    if (qNumber == questions.length - 1) {
+      return <div>You good at the Game, don't you</div>;
+    }
+    return (
+      <div id="question" className="w-screen flex justify-center items-center">
+        {questions[qNumber + 1].q}
+      </div>
+    );
+  }, [qNumber]);
 
-const Question = () => {
-  return <div className="h-screen">question</div>;
+  const handlePageChange = () => {
+    document.querySelectorAll("#question").forEach((e) => {
+      e.classList.add(styles.changeQuestion);
+    });
+    if (qNumber == questions.length) {
+      handleEndQuiz();
+      return;
+    }
+    setTimeout(() => {
+      document.querySelectorAll("#question").forEach((e) => {
+        setQNumber(qNumber + 1);
+        e.classList.remove(styles.changeQuestion);
+      });
+    }, 1000);
+  };
+
+  const handleEndQuiz = () => {};
+  return (
+    <div className={styles.questionScreen} {...props}>
+      <button
+        onClick={() => handlePageChange()}
+        className="absolute right-4 top-1/2 -translate-y-1/4"
+      >
+        <FontAwesomeIcon icon={faAngleRight} />
+      </button>
+      {renderLeft}
+      {renderRight}
+    </div>
+  );
 };
 
 export default Question;
