9051ccb2d582afbd6ae1d381160be8b4b3c351d0
tententgc <nyman3527@gmail.com>
2022-06-30 17:52:55 +0700

    update  call json

--- quizzzzz/components/Question.jsx
+++ quizzzzz/components/Question.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Question = () => {
-  return <div className="h-screen">question</div>;
+
+export const getStaticProps = async () => { 
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    return { 
+      props : { quiz : data }
+    }
+
+
+}
+const Question = ({quiz}) => {
+  console.log(quiz)
+  return <div className="h-screen"> {quiz.name}</div>;
 };
 
 export default Question;
